f79e3f8242eefe76a0ad9ed25b0344f8c42c0ff6
jsendz <joshuasendzul@gmail.com>
2026-06-02 18:04:50 +0200

    gos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,18 @@ export const metadata: Metadata = {
   title: "Top Gos – Your Trusted Pet Caregivers",
   description:
     "Professional dog walking and grooming services. Tailored care for your furry companions, 24/7.",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico" },
+    ],
+    apple: { url: "/apple-touch-icon.png" },
+    other: [
+      { rel: "android-chrome-192x192", url: "/android-chrome-192x192.png" },
+      { rel: "android-chrome-512x512", url: "/android-chrome-512x512.png" },
+    ],
+  },
 };
 
 export default function RootLayout({
